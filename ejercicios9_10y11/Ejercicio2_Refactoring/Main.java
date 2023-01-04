2dec01fcc2ae6cf531e7636aa22f3e4c88393128
EltonJitiako <eltonjitiako@gmail.com>
2023-01-04 10:07:39 +0100

    commit 9,10 y 11 - 5

--- ejercicios9_10y11/Ejercicio2_Refactoring/Main.java
+++ ejercicios9_10y11/Ejercicio2_Refactoring/Main.java
@@ -1,23 +1,50 @@
-package sesion7.Prototype;
+package ejercicio4;
 
+/**
+ * Ejercicios 9, 10 y 11 - ejercicio 2 Java Avanzado
+ * Ejercicio extraido de ejercicio 4 Java Basico - "En este ejercicio tendréis que crear una clase SmartDevice. Dentro crearéis las clases hijas: SmartPhone y SmartWatch. Agregaréis atributos tal cual tendrían esos objetos en la realidad. Crear constructor vacío y con todos los parámetros para cada clase. Desde una clase Main: crearéis objetos de cada una y los utilizaréis para imprimir sus valores por consola."
+ * *********************************************************
+ *
+ */
 public class Main {
-    public static void main (String []args) {
-        Vehiculo vehiculo = new Vehiculo();
-        vehiculo.marca = "Ford";
-        vehiculo.modelo = "Mondeo";
+    public static void main(String[] args) {
+        
+        SmartPhone mobile1 = new SmartPhone();
 
-        Vehiculo vehiculo2 = vehiculo.clonar();
-        System.out.println(vehiculo2.marca);
-        vehiculo2.marca = "Honda";
-        System.out.println(vehiculo2.marca);
+        System.out.println(mobile1.marca + ", "
+                            + mobile1.modelo + ", "
+                            + mobile1.pantalla + ", "
+                            + mobile1.peso + ", "
+                            + mobile1.precio + ", "
+                            + mobile1.simCard);
 
-        Vehiculo vehiculo3 = vehiculo.clonar();
-        System.out.println(vehiculo3.marca);
+        SmartPhone mobile2 = new SmartPhone("Apple", "iPhone14" , 6.7, 300.0, 1000.00, "eSim");
 
-        Moto moto = new Moto();
-        moto.setCilindrada(150);
+        System.out.println(mobile2.marca + ", "
+                            + mobile2.modelo + ", "
+                            + mobile2.pantalla + ", "
+                            + mobile2.peso + ", "
+                            + mobile2.precio + ", "
+                            + mobile2.simCard);
 
-        Moto moto2 = moto.clonar();
-        System.out.println("La cilindrada de moto2 es: " + moto2.getCilindrada());
+        SmartWatch watch1 = new SmartWatch();
+
+        System.out.println(watch1.marca + ", "
+                            + watch1.modelo + ", "
+                            + watch1.pantalla + ", "
+                            + watch1.peso + ", "
+                            + watch1.precio + ", "
+                            + watch1.colorCorrea);
+
+        SmartWatch watch2 = new SmartWatch("Samsung", "Galaxy watch 4", 2.0, 60.0, 400.00, "gris");
+
+        System.out.println(watch2.marca + ", "
+                            + watch2.modelo + ", "
+                            + watch2.pantalla + ", "
+                            + watch2.peso + ", "
+                            + watch2.precio + ", "
+                            + watch2.colorCorrea);
+
     }
 }
+
