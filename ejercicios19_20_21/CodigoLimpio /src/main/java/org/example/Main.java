1d3ef377dca6404e3128171076e5f2b166b6db76
EltonJitiako <eltonjitiako@gmail.com>
2023-01-10 10:37:22 +0100

    commit 19 20 21 - 2

---
+++ ejercicios19_20_21/CodigoLimpio /src/main/java/org/example/Main.java
@@ -0,0 +1,118 @@
+package org.example;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+    
+        public static void main(String[] args) {
+
+            String file = "file.txt";
+
+            try {
+
+                fileInOut(file);
+
+            } catch(Exception e) {
+
+                System.out.println("Exception IO or File Nor Found: " + e.getMessage());
+
+            }
+
+        }
+
+        private static void fileInOut(String file) throws IOException {
+
+            try {
+
+                InputStream in = new FileInputStream(file);
+                byte[] datos = in.readAllBytes();
+                in.close();
+
+                ArrayList<String> nombresList = pasaArray(datos);
+
+                nombresList = pasaApNombres(nombresList);
+
+                datos = pasaByte(nombresList);
+
+                PrintStream out = new PrintStream(file);
+                out.write(datos);
+                out.close();
+
+            } catch (Exception e) {
+
+                throw new IOException();
+
+            }
+
+        }
+
+        private static byte[] pasaByte(ArrayList<String> nombresList) {
+
+            String listString = "";
+            for (String nombre : nombresList) {
+
+                listString += nombre + ", ";
+
+            }
+
+            System.out.println("Despues: " + listString);
+            byte[] novoDatos = listString.getBytes(StandardCharsets.UTF_8);
+
+            return (novoDatos);
+        }
+
+        private static ArrayList<String> pasaApNombres(ArrayList<String> nombresList) {
+
+            HashMap<String, String> apNombres = new HashMap<>();
+
+            apNombres.put("Maria", "Gonzalez");
+            apNombres.put("Lucas", "Valle");
+            apNombres.put("Mateus", "Gimenez");
+            apNombres.put("Icaro", "Perez");
+            apNombres.put("Luciane", "Luz");
+            apNombres.put("Ariane", "Farina");
+            apNombres.put("Cintia", "Gasp");
+            apNombres.put("Jonas", "Herrera");
+
+            for (int i=nombresList.size()-1; i >= 0 ; i--) {
+
+                nombresList.set( nombresList.indexOf(nombresList.get(i)) , nombresList.get(i) + ' ' + apNombres.get(nombresList.get(i)));
+
+            }
+
+            return nombresList;
+
+        }
+
+        private static ArrayList<String> pasaArray(byte[] datos) {
+
+            ArrayList<String> nombresList = new ArrayList<>();
+            String nombresString = new String(datos, StandardCharsets.UTF_8);
+            System.out.println("Antes: " + nombresString);
+            String nombre = "";
+            for (char i : nombresString.toCharArray()) {
+
+                if(i != ',' && i != ' ') {
+
+                    nombre += i;
+
+                } else if(i == ',') {
+
+                    nombresList.add(nombre);
+                    nombre="";
+
+                }
+
+            }
+
+            return nombresList;
+
+        }
+
+}
